8d5f031a49c85378a209f1c6e9bfe0388fea78f2
ricardosoriagalvarroguerra <110310949+ricardosoriagalvarroguerra@users.noreply.github.com>
2024-11-22 09:16:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.subheader("Chatbot con PDF y GroqCloud", divider="🌈", anchor=False)
 
 # Inicializar el cliente de GroqCloud
 client = Groq(
-    api_key=st.secrets["GROQ_API_KEY"],
+    api_key=st.secrets["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"],
 )
 
 # Inicializar EasyOCR
